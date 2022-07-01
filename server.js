45a383404e5c4ed51441626a64f0df72e52123cf
yonatankumsa <yonatankumsa99@gmail.com>
2022-07-01 11:58:18 -0400

    updated read me

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
+///
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
