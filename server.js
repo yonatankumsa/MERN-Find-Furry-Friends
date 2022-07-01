0fb005ac526b308f5c5b0b0772291de42e6b91cf
yonatankumsa <yonatankumsa99@gmail.com>
2022-07-01 13:12:18 -0400

    trying to fix updated read me

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/api/users', require('./routes/api/users'))
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+////
 ///
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
