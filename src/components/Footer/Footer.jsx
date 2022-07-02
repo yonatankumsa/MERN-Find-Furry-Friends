77d27a096bedcc1fa1b81ab30b2b7ef5417b6ac1
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-07-02 12:10:30 -0400

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      {/* Add each one's GitHub link later */}
+      Copyright &copy; Michalle Khan & Yonatan Kumsa & Wenling Yu 2022
+    </footer>
+  );
+}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,31 @@
-import './NavBar.css'
-import { Link } from 'react-router-dom'
-import * as userService from '../../utilities/users-service'
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import * as userService from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser }) {
-    
-    // Add the following function
-    function handleLogOut() {
-        // Delegate to the users-service
-        userService.logOut()
-        // Update state will also cause a re-render
-        setUser(null)
-    }
+  // Add the following function
+  function handleLogOut() {
+    // Delegate to the users-service
+    userService.logOut();
+    // Update state will also cause a re-render
+    setUser(null);
+  }
 
-    return(
-        <nav>
-            <Link to="/orders">Order History</Link>
-            &nbsp; | &nbsp;
-            <Link to="/orders/new">New Order</Link>
-            &nbsp; | &nbsp;
-            Welcome, { user.name }
-            &nbsp; | &nbsp;
-            <Link to="" onClick={handleLogOut}>Log Out</Link>
-        </nav>
-    )
+  return (
+    <nav>
+      {/* TODO: to update the link?? */}
+      <Link to="/findfurryfriends">All Posts</Link>
+      &nbsp; | &nbsp;
+      {/* TODO: to update the link?? */}
+      <Link to="/findfurryfriends/new">New Post</Link>
+      &nbsp; | &nbsp; Welcome, {user.name}
+      &nbsp; | &nbsp;
+      {/* TODO: to update the link?? */}
+      <Link to="/findfurryfriends/:userName">My Account</Link>
+      &nbsp; | &nbsp;
+      <Link to="" onClick={handleLogOut}>
+        Log Out
+      </Link>
+    </nav>
+  );
 }
