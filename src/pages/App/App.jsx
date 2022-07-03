c29077d469ae79e117ad97d4ca3526cc22662481
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-07-03 15:15:17 -0400

    update petDetailsPage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -9,6 +9,7 @@ import NewPostPage from "../NewPostPage/NewPostPage";
 import FoundPostsPage from "../FoundPostsPage/FoundPostsPage";
 import LostPostsPage from "../LostPostsPage/LostPostsPage";
 import UsersPage from "../../pages/UsersPage/UsersPage";
+import PetDetailsPage from "../../pages/PetDetailsPage/PetDetailsPage";
 // Components
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
@@ -31,6 +32,8 @@ export default function App() {
             <Route path="/LostPosts" element={<LostPostsPage />} />
             <Route path="/NewPost" element={<NewPostPage />} />
             <Route path="/myaccount" element={<UsersPage user={user} />} />
+            {/* pet id ....how to get it? */}
+            <Route path="/:petId" element={<PetDetailsPage />} />
             {/* redirect to /AllPostt if path in address bar hasn't matched a <Route> above */}
             <Route path="/*" element={<Navigate to="/AllPosts" />} />
           </Routes>
