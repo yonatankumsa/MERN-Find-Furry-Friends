1b03c7d57cce5ddbbd10dd52ed696bc9074e297e
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-07-03 14:49:10 -0400

    update the AllPostsPage that we can jump to lost/found page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,7 +6,8 @@ import { getUser } from "../../utilities/users-service";
 import AuthPage from "../AuthPage/AuthPage";
 import AllPostsPage from "../AllPostsPage/AllPostsPage";
 import NewPostPage from "../NewPostPage/NewPostPage";
-
+import FoundPostsPage from "../FoundPostsPage/FoundPostsPage";
+import LostPostsPage from "../LostPostsPage/LostPostsPage";
 import UsersPage from "../../pages/UsersPage/UsersPage";
 // Components
 import NavBar from "../../components/NavBar/NavBar";
@@ -14,13 +15,20 @@ import Footer from "../../components/Footer/Footer";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+  const [comments, setComments] = useState("this a fake comment");
+
   return (
     <main className="App">
       {user ? (
         <>
           <NavBar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/AllPosts" element={<AllPostsPage />} />
+            <Route
+              path="/AllPosts"
+              element={<AllPostsPage comments={comments} />}
+            />
+            <Route path="/FoundPosts" element={<FoundPostsPage />} />
+            <Route path="/LostPosts" element={<LostPostsPage />} />
             <Route path="/NewPost" element={<NewPostPage />} />
             <Route path="/myaccount" element={<UsersPage user={user} />} />
             {/* redirect to /AllPostt if path in address bar hasn't matched a <Route> above */}
