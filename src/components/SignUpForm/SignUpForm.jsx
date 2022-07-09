611f0ddf2191f9193255e2e828f0a2cc4bdab051
mkhan42 <michallekhan9@gmail.com>
2022-07-08 23:05:24 -0400

    heroku

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import React from "react";
 import { signUp } from "../../utilities/users-service.js";
+import "./SignUpForm.css";
 
 export default class SignUpForm extends Component {
   state = {
@@ -43,53 +44,95 @@ export default class SignUpForm extends Component {
     const disable = this.state.password !== this.state.confirm;
     return (
       <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Profile Image</label>
-            <input
-              type="text"
-              name="userProfileImg"
-              value={this.state.userProfileImg}
-              onChange={this.handleChange}
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Confirm</label>
-            <input
-              type="password"
-              name="confirm"
-              value={this.state.confirm}
-              onChange={this.handleChange}
-              required
-            />
-            <button type="submit" disabled={disable}>
-              SIGN UP
-            </button>
-          </form>
+        <div className="card my-sign-card">
+          <div className="card-body my-sign-card-body">
+            <form autoComplete="off" onSubmit={this.handleSubmit}>
+              <div className="mb-3 my-sign-div">
+                <label htmlFor="exampleInputEmail1" className="form-label my-sign-label" >
+                  Name
+                </label>
+                <input
+                  className="form-control my-sign-input"
+                  id="exampleInputEmail1"
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                  placeholder="Ex: John Doe"
+                  required
+                />
+              </div>
+              <br></br>
+              <div className="mb-3 my-sign-div">
+                <label htmlFor="exampleInputEmail1" className="form-label my-sign-label">
+                  Email address
+                </label>
+                <input
+                  className="form-control my-sign-input"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <br></br>
+              <div className="mb-3 my-sign-div">
+                <label htmlFor="exampleInputEmail1" className="form-label my-sign-label">
+                  Profile Image
+                </label>
+                <input
+                  className="form-control my-sign-input"
+                  id="exampleInputEmail1"
+                  type="text"
+                  name="userProfileImg"
+                  value={this.state.userProfileImg}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <br></br>
+              <div className="mb-3 my-sign-div">
+                <label htmlFor="exampleInputPassword1" className="form-label my-sign-label">
+                  Password
+                </label>
+                <input
+                  className="form-control my-sign-input"
+                  id="exampleInputPassword1"
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <br></br>
+              <div className="mb-3 my-sign-div">
+                <label htmlFor="exampleInputPassword1" className="form-label my-sign-label">
+                  Confirm
+                </label>
+                <input
+                  className="form-control my-sign-input"
+                  id="exampleInputPassword1"
+                  type="password"
+                  name="confirm"
+                  value={this.state.confirm}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={disable}
+              >
+                SIGN UP
+              </button>
+            </form>
+            <p className="error-message">&nbsp;{this.state.error}</p>
+          </div>
         </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
       </div>
     );
   }
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import React from "react";
 
 export default function AuthPage({ setUser }) {
+
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
-    <main>
-      <h1>AuthPage</h1>
-      <SignUpForm setUser={setUser} />
-      <LoginForm setUser={setUser} />
-    </main>
+    <>
+    <h1>FIND-FURRY-FRIENDS</h1>
+     {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      <div>
+      <p onClick={() => setShowLogin(!showLogin)}>{showLogin ? `Don't have an account? Sign up!` : 'Already have an account? Log in'}</p>
+      </div>
+    </>
   );
 }
