581bbf9ff7f24537aa6070823c4d488e7c660310
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-07-09 10:21:37 -0400

    search bar works

--- src/pages/FoundPostsPage/FoundPostsPage.jsx
+++ src/pages/FoundPostsPage/FoundPostsPage.jsx
@@ -1,17 +1,49 @@
 import PetCard from "../../components/PetCard/PetCard";
-import React from "react";
+import { useState } from "react";
+import { Card, Input } from "semantic-ui-react";
+
 export default function FoundPostsPage({ posts }) {
   // data with postType=Found
   const foundData = posts.filter((p) => p.postType === "Found");
   // console.log(foundData);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  function searchItem(value) {
+    setSearchInput(value);
+    if (searchInput !== 0) {
+      const filteredData = foundData.filter((post) => {
+        return Object.values(post.animalType)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    }
+  }
+
   return (
     <>
-      <h1>this is FoundPostsPage</h1>
+      <h1>Found Posts</h1>
+      <Input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => {
+          searchItem(e.target.value);
+        }}
+      />
+      <div></div>
       {foundData.length ? (
         <div>
-          {foundData.map((post) => {
-            return <PetCard key={post._id} post={post} />;
-          })}
+          <Card.Group>
+            {searchInput.length === 0
+              ? foundData.map((post) => {
+                  return <PetCard key={post._id} post={post} />;
+                })
+              : filteredResults.map((post) => {
+                  return <PetCard key={post._id} post={post} />;
+                })}
+          </Card.Group>
         </div>
       ) : (
         <h2>No Found Post</h2>
