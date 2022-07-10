9b4c87cabff5740e9117e105d8b84857f360ca24
yonatankumsa <yonatankumsa99@gmail.com>
2022-07-10 14:37:13 -0400

    all page header,footer,navbar

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -4,12 +4,12 @@ import "./PetCard.css"
 export default function PetCard({ post }) {
   let petURL = `/${post._id}`;
   return (
-   <div className="PetCarddiv">
+  
       <Link to={petURL}>
         {/* <p>{post.postTitle}</p> */}
         <img src={post.imgURL} alt={post.name} className="animalImg" />
         <h2 className="PetCardh2">{post.name}</h2>
       </Link>
-      </div>
+     
   );
 }
