fd43f1ecdc6655cd450d836facdc5a6641de4e6e
mkhan42 <michallekhan9@gmail.com>
2022-07-09 20:37:44 -0400

    made changes

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -43,26 +43,10 @@ export default class SignUpForm extends Component {
   render() {
     const disable = this.state.password !== this.state.confirm;
     return (
-
-//       <div class="card mb-3" style="max-width: 540px;">
-//   <div class="row g-0">
-//     <div class="col-md-4">
-//       <img src="..." class="img-fluid rounded-start" alt="...">
-//     </div>
-//     <div class="col-md-8">
-//       <div class="card-body">
-//         <h5 class="card-title">Card title</h5>
-//         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//       </div>
-//     </div>
-//   </div>
-// </div>
       <div className="my-sign-back-div">
         <div className="card my-sign-card mb-3">
           <div className="row g-0 my-sign-row">
             <div className="col-md-6 my-img-col" id="adding-opacity">
-              {/* <img src="https://images.unsplash.com/photo-1601758177266-bc599de87707?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" className="my-sign-img" alt="..."/> */}
               <div className="my-auth-p">
               <h5 className="site-description">Lost a furry friend? Found a lost friend? <br/> <br/>Find-Furry-Friends is dedicated to helping pet owners reunite with their missing friends. <br/> <br/>Just Sign Up or Log in!</h5>
               </div>
@@ -150,7 +134,7 @@ export default class SignUpForm extends Component {
               <div className="my-btn-sign">
               <button
                 type="submit"
-                className="btn btn-primary"
+                className="my-btn btn btn-primary"
                 disabled={disable}
               >
                 SIGN UP
